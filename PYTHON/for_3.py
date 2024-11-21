06c692c36d61d44aad7f02df9eb793ee87aa6a79
Fernanda Batista <fernandinhabatista78@hotmail.com>
2024-11-21 17:12:26 -0300

    Atividade de python

---
+++ PYTHON/for_3.py
@@ -0,0 +1,11 @@
+#Pedir cinco números para o usuário
+
+num = []
+for p in range(1,6):
+    v = int(input(f"Digite o {p}º número: "))
+    # Adicionar o valor de v na lista num
+
+    num.append(v)
+   
+for x in num:
+    print(x, end=" - ")
---
+++ PYTHON/funcao3.py
@@ -0,0 +1,13 @@
+def area_retangulo(altura=0, largura=0):
+    """"
+    A função area_retangulo calcula a área de um retangulo,
+    usando os valores de altura e largura. E retorna o produto 
+    dos valores
+
+     """
+   
+    rs = altura * largura 
+    return rs 
+
+
+print(area_retangulo(20,30))
